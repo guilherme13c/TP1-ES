66fa64c70720e13c04a205389a2ca2e8201c25f8
Guilherme Caporali <guilhermecaporali504@gmail.com>
2023-05-05 15:06:21 -0300

    Restored DataStructures.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS  # comment this on deployment
+from api.HelloApiHandler import HelloApiHandler
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app)  # comment this on deployment
@@ -11,3 +12,5 @@ api = Api(app)
 def serve(path):
     return send_from_directory(app.static_folder, 'index.html')
 
+
+api.add_resource(HelloApiHandler, '/flask/hello')
