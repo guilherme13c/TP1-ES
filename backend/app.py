15e8b81f278bf5aa5e1339dfd7dc41381c40fe58
Guilherme Caporali <guilhermecaporali504@gmail.com>
2023-05-06 16:26:14 -0300

    started creating login page

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,33 @@
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3000/login",
+    "http://localhost:3000/register",
+    " http://192.168.1.121:3000/"
+           ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+class LoginResponse(BaseModel):
+    authorize: bool
+
 @app.get('/')
 async def root():
     return {"message": "Hello, World!"}
+
+@app.get('/login', status_code=200)
+async def login():
+    return {
+        "valid": True, 
+        }
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+    const [formData, setFormData] = useState({username: "", password: ""});
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({...prevFormData, [name]: value}));
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        // TODO: Consume login API
+    }
+
+    // TODO: style page
     return (
-        <h1>this is the login page!</h1>
+        <form onSubmit={handleSubmit}>
+            <label>Usuário
+                <input type='text' name='username' value={formData.username} onChange={handleChange}/>
+            </label>
+            <label>Senha
+                <input type='password' name='password' value={formData.password} onChange={handleChange}/>
+            </label>
+            <button type='submit'>Login</button>
+        </form>
     );
 }
 
