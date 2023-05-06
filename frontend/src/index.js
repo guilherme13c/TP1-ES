4aeb3b65f64e224dc6d12ae8908d187b3ec0107f
Guilherme Caporali <guilhermecaporali504@gmail.com>
2023-05-06 09:47:45 -0300

    project structure created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
+import React from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Register from "./pages/register";
+import Login from "./pages/login";
+import Home from "./pages/home";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login/" element={<Login />}/>
+        <Route path="/register/" element={<Register />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ frontend/src/pages/register.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Register() {
+    return (
+        <h1>this is the sign up page!</h1>
+    );
+}
+
+export default Register;
