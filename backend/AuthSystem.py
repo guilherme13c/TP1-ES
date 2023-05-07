17c10f617fdfa9cbbcf1949a63240a61e79788ab
Guilherme Caporali <guilhermecaporali504@gmail.com>
2023-05-07 12:50:00 -0300

    bug fix

--- backend/AuthSystem.py
+++ backend/AuthSystem.py
@@ -1,18 +1,20 @@
 from passlib.context import CryptContext
-import DatabaseInterface
+from DatabaseInterface import *
 from DataStructures import User
 
 db = DatabaseInterface()
 
+
 def verifyUser(email: str, password: str):
 
-    user = db.get_user(email)    
+    user = db.get_user(email)
     if user:
         if user.password == password:
             return user
         return False
     else:
         return None
+
 
 def registerUser(email: str, password: str, name: str, gender: str, course: str, neighborhood: str):
     if db.get_user(email):
