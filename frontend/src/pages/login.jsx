9dc317a0c1b9cc2173669ebdb508cd8c666a8857
Guilherme Caporali <guilhermecaporali504@gmail.com>
2023-05-07 12:54:28 -0300

    sync

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -12,7 +12,7 @@ function Login() {
         event.preventDefault();
 
         try {
-            const response = await fetch("http://127.0.0.1:8000/login", {
+            const response = await fetch("http://127.0.0.1:8080/login", {
                 method: "POST",
                 headers: {
                     'Content-Type': 'application/json'
