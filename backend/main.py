05d79e29d6491767fdcc8834b24ea0192f3d0291
a <vitorfhsouza@gmail.com>
2023-05-08 18:21:01 -0300

    API get_rides e add_ride

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from AuthSystem import *
+from DatabaseInterface import *
+from typing import List
 
 app = FastAPI()
+db = DatabaseInterface()
 
 origins = [
     "http://localhost:3000",
@@ -38,6 +41,14 @@ class RegisterFormData(BaseModel):
     neighbourhood: str
 
 
+class RideFormData(BaseModel):
+    orig: str
+    dest: str
+    time: str
+    days: List[int]
+    seats_offered: int
+
+
 @app.post('/login')
 async def login(login_form_data: LoginFormData):
     user = authenticateUser(
@@ -65,6 +76,16 @@ async def login(register_form_data: RegisterFormData):
         return {
             "email": register_form_data.email,
         }
+    
+@app.get('/get_rides')
+async def login():
+    rides=db.get_all_rides()
+    return rides
+
+@app.post('/add_ride')
+async def login(rfd: RideFormData):
+    rides=db.add_ride(0,rfd.orig,rfd.dest,rfd.time,rfd.days,rfd.seats_offered);
+    return rides
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8080, log_level='info')
