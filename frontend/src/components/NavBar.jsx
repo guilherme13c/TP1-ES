bc3733c9a4c875c52828b86f29d2c382a3aee036
a <vitorfhsouza@gmail.com>
2023-05-09 21:53:10 -0300

    Estilizar rides

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,7 +4,13 @@ function Navbar() {
   return (
     <nav className="navbar">
       <ul>
+        <li>
+          <h2>FaculRide</h2>
+        </li>
         <li className="right">
+          <a href="/">Home</a>
+        </li>
+        <li className="left">
           <a href="/rides">Caronas</a>
         </li>
         <li className="left">
